7764a92b0f25ecb6a3d1149ee5adfe8764cafc36
Jakub Stepnik <jakubstepni1k@gmail.com>
2023-03-18 22:18:27 +0100

    add tailwind and basic styling

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    colors: {
+      blue: {
+        100: "hsl(227, 12%, 61%)",
+        200: "hsl(228, 39%, 23%)",
+        300: "hsl(233, 12%, 13%)",
+      },
+      red: {
+        100: "hsl(12, 88%, 59%)",
+        200: "hsl(13, 100%, 96%)",
+        300: "hsl(233, 12%, 13%)",
+        grey: {
+          100: "hsl(0, 0%, 98%)",
+        },
+      },
+    },
+    fontFamily: {
+      sans: ['"Be Vietnam Pro"'],
+    },
+  },
+  plugins: [],
+};
